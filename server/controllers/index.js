45bd86463c291046a102ca9d346bec0397760397
tobeidi <tobeidi@ymail.com>
2022-10-28 13:02:18 -0400

    Modifying the home page

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,3 +1,3 @@
 export function DisplayHomePage(req, res, next) {
-    res.render('index', { title: 'Home', page: 'home' });
+    res.render('index', { title: 'Home Page - MidTerm for Tarik Obeidi - 301252585', page: 'home' });
 }
